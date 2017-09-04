9b671c55a92667ffa8082af3f941245515977ec3
Zib3l <zib3l@hotmail.com>
2017-09-04 16:12:21 +0200

    implementacja

---
+++ src/Zib3l/Zib3lContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zib3l
+{
+    public class Zib3lContainer
+    {
+        readonly Dictionary<Type, Type> _registrations = new Dictionary<Type, Type>();
+
+        public void Register<Tregistration, TImplementation>()
+        {
+            _registrations.Add(typeof(Tregistration), typeof(TImplementation));
+        }
+
+        public object Resolve<T>()
+        {
+            var request_type = typeof(T);
+            Type actual_type = _registrations[request_type];
+            var instance = Activator.CreateInstance(actual_type);
+            return (T) instance;
+        }
+    }
+}
